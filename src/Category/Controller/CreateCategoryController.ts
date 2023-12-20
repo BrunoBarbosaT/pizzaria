d7d0048b24a3432e4772dd61284de4e447ca8ed8
bruno.barbosa <engenheiro.brunobarbosateixeira@gmail.com>
2023-12-20 20:27:32 -0300

    [category] - adicionanado create e list de category

---
+++ src/Category/Controller/CreateCategoryController.ts
@@ -0,0 +1,16 @@
+import { Request, Response} from  'express'
+import { CreateCategoryService } from '../Service/CreateCategoryService'
+
+class CreateCategoryController{
+    async handle(req: Request, res: Response){
+
+    const { name } = req.body;
+
+        const createCategoryService = new CreateCategoryService();
+        const category = await createCategoryService.execute({name});
+
+        return res.json(category);
+    }
+}
+
+export { CreateCategoryController }
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import { Router} from "express"
 
+import { isAutenticated } from "./Middilawere/isAuthenticated"
+
 import { CreateUserController } from './Users/Controller/CreateUserController'
-import { AuthUSerController } from "./Users/Controller/AuthUserController";
-import { DetailUserController } from "./Users/Controller/DetailUserController";
-import { isAutenticated } from "./Middilawere/isAuthenticated";
+import { AuthUSerController } from "./Users/Controller/AuthUserController"
+import { DetailUserController } from "./Users/Controller/DetailUserController"
+import { CreateCategoryController } from "./Category/Controller/CreateCategoryController"
+import { ListCategoryController } from "./Category/Controller/ListCategoryController"
+
+
 const router = Router();
 
 
@@ -11,5 +16,9 @@ const router = Router();
 router.post('/users', new CreateUserController().headle)
 router.post('/session', new AuthUSerController().handle)
 router.get('/details',isAutenticated, new DetailUserController().handle)
+
+//--ROTAS CATEGORY --//
+router.post('/category', isAutenticated, new CreateCategoryController().handle)
+router.get('/category',  isAutenticated, new ListCategoryController().handle) 
 
 export{ router}
